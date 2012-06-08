fae2e64795866eefc818f075f0b6e6f7e801b0ff
Lars <Lars@localhost>
2012-06-08 22:36:26 +0200

    Docs

--- mql.tolog/mql/tolog/utils.py
+++ mql.tolog/mql/tolog/utils.py
@@ -32,7 +32,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 """\
-
+Internal utility functions.
 
 :author:       Lars Heuer (heuer[at]semagia.com)
 :organization: Semagia - <http://www.semagia.com/>
