a340e955661e5786d6b966cf93d8ddc4d599c03e
Lars <Lars@localhost>
2014-07-08 00:28:05 +0200

    Updated file header, PEP8

--- mappa.xtm/tests/test_xtm.py
+++ mappa.xtm/tests/test_xtm.py
@@ -1,35 +1,9 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2007 - 2011 -- Lars Heuer - Semagia <http://www.semagia.com/>.
+# Copyright (c) 2007 - 2014 -- Lars Heuer - Semagia <http://www.semagia.com/>.
 # All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#
-#     * Redistributions in binary form must reproduce the above
-#       copyright notice, this list of conditions and the following
-#       disclaimer in the documentation and/or other materials provided
-#       with the distribution.
-#
-#     * Neither the name of the project nor the names of the contributors 
-#       may be used to endorse or promote products derived from this 
-#       software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# BSD license.
 #
 """\
 Tests against the XTM writers.
@@ -43,8 +17,10 @@ from mio.xtm import create_deserializer
 from mappaext import xtm
 from mappa.xtm1utils import convert_to_tmdm
 
+
 def create_xtm10_writer(out, base):
     return xtm.create_writer(out, base, prettify=True, version=1.0)
+
 
 def create_xtm20_writer(out, base):
     return xtm.create_writer(out, base, prettify=True, version=2.0)
@@ -108,10 +84,12 @@ _EXCLUDE_XTM_20 = [
     'merge-itemid-with-types.xtm',
     ]
 
+
 def test_xtm_10_writer():
     for test in create_writer_cxtm_cases(create_xtm10_writer, create_deserializer, 'xtm1', 'xtm', post_process=convert_to_tmdm,
                                          exclude=_EXCLUDE_XTM_10):
         yield test
+
 
 def test_xtm_20_writer():
     for test in create_writer_cxtm_cases(create_xtm20_writer, create_deserializer, 'xtm1', 'xtm', post_process=convert_to_tmdm,
@@ -120,6 +98,7 @@ def test_xtm_20_writer():
     for test in create_writer_cxtm_cases(create_xtm20_writer, create_deserializer, 'xtm2', 'xtm',
                                          exclude=_EXCLUDE_XTM_20):
         yield test
+
 
 def test_xtm_21_writer():
     for test in create_writer_cxtm_cases(create_xtm21_writer, create_deserializer, 'xtm1', 'xtm', post_process=convert_to_tmdm):
