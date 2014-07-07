8507d5d317ada3c70345634d3ac2b6e2c25573c5
Lars <Lars@localhost>
2014-07-08 00:25:21 +0200

    Updated file header, PEP8, removed fallback to django.utils.simplejson

--- mio.jtm/mio/jtm/json.py
+++ mio.jtm/mio/jtm/json.py
@@ -1,35 +1,9 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2007 - 2011 -- Lars Heuer - Semagia <http://www.semagia.com/>.
+# Copyright (c) 2007 - 2014 -- Lars Heuer - Semagia <http://www.semagia.com/>.
 # All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#
-#     * Redistributions in binary form must reproduce the above
-#       copyright notice, this list of conditions and the following
-#       disclaimer in the documentation and/or other materials provided
-#       with the distribution.
-#
-#     * Neither the project name nor the names of the contributors may be 
-#       used to endorse or promote products derived from this software 
-#       without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# BSD license.
 #
 """\
 This module provides a JSON writer.
@@ -42,72 +16,12 @@ from __future__ import absolute_import
 try:
     import simplejson as json
 except ImportError:
-    try:
-        import json
-    except ImportError:
-        try:
-            # Google Appengine offers simplejson via django
-            from django.utils import simplejson as json
-        except ImportError:
-            raise ImportError('Cannot find a JSON module')
+    import json
+from json.encoder import encode_basestring_ascii as escape
 
 load = json.load
 loads = json.loads
 
-# Try to import some (json|simplejson) specific stuff. 
-# If json or simplejson is available the JSONWriter may operate faster 
-# (at least if the C version of the encoder is available)
-#pylint: disable-msg=E0102
-found = False
-try:
-    #pylint: disable-msg=E0611, F0401
-    from json.encoder import encode_basestring_ascii as escape
-    found = True
-except ImportError:
-    pass
-if not found:
-    # Code from simplejson.encoder Copyright (c) Bob Ippolito
-    # License: MIT
-    # http://undefined.org/python/#simplejson
-    import re
-    ESCAPE = re.compile(r'[\x00-\x1f\\"\b\f\n\r\t]')
-    ESCAPE_ASCII = re.compile(r'([\\"]|[^\ -~])')
-    HAS_UTF8 = re.compile(r'[\x80-\xff]')
-    ESCAPE_DCT = {
-        '\\': '\\\\',
-        '"': '\\"',
-        '\b': '\\b',
-        '\f': '\\f',
-        '\n': '\\n',
-        '\r': '\\r',
-        '\t': '\\t',
-        }
-    for i in range(0x20):
-        #ESCAPE_DCT.setdefault(chr(i), '\\u{0:04x}'.format(i))
-        ESCAPE_DCT.setdefault(chr(i), '\\u%04x' % (i,))
-    def escape(s):
-        """\
-        Return an ASCII-only JSON representation of a Python string
-        """
-        if isinstance(s, str) and HAS_UTF8.search(s) is not None:
-            s = s.decode('utf-8')
-        def replace(match):
-            s = match.group(0)
-            try:
-                return ESCAPE_DCT[s]
-            except KeyError:
-                n = ord(s)
-                if n < 0x10000:
-                    #return '\\u{0:04x}'.format(n)
-                    return '\\u%04x' % (n,)
-                else:
-                    # surrogate pair
-                    n -= 0x10000
-                    s1 = 0xd800 | ((n >> 10) & 0x3ff)
-                    s2 = 0xdc00 | (n & 0x3ff)
-                    #return '\\u{0:04x}\\u{1:04x}'.format(s1, s2)
-                    return '\\u%04x\\u%04x' % (s1, s2)
-        return '"' + str(ESCAPE_ASCII.sub(replace, s)) + '"'
 
 class JSONWriter(object):
     """\
