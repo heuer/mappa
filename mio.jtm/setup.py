0bb4b60783fef50979d4420a63596e93ac2a8198
Lars <Lars@localhost>
2014-07-09 00:37:57 +0200

    Updated CHANGES, PEP8

--- mio.jtm/setup.py
+++ mio.jtm/setup.py
@@ -16,34 +16,34 @@ except ImportError:
     from setuptools import setup, find_packages
 
 setup(
-      name = 'mio.jtm',
-      version = '0.1.2',
-      description = 'JSON Topic Maps (JTM) syntax reader',
-      long_description = '\n\n'.join([open('README.txt').read(), open('CHANGES.txt').read()]),
-      author = 'Lars Heuer',
-      author_email = 'mappa@googlegroups.com',
-      url = 'http://mappa.semagia.com/',
-      license = 'BSD',
-      packages = find_packages(),
-      namespace_packages = ['mio'],
-      entry_points = """
-      [mio.reader]
-      jtm = mio.jtm
-      """,
-      platforms = 'any',
-      zip_safe = False,
-      include_package_data = True,
-      package_data = {'': ['*.txt']},
-      install_requires=['tm>=0.1.7'],
-      keywords = ['Topic Maps', 'Semantic Web', 'JTM', 'JSON Topic Maps'],
-      classifiers = [
-                    'Intended Audience :: Developers',
-                    'Intended Audience :: Information Technology',
-                    'Topic :: Software Development',
-                    'Topic :: Software Development :: Libraries',
-                    'Topic :: Software Development :: Libraries :: Python Modules',
-                    'License :: OSI Approved :: BSD License',
-                    'Operating System :: OS Independent',
-                    'Programming Language :: Python',
-                    ]
-      )
+    name='mio.jtm',
+    version='0.1.2',
+    description='JSON Topic Maps (JTM) syntax reader',
+    long_description='\n\n'.join([open('README.txt').read(), open('CHANGES.txt').read()]),
+    author='Lars Heuer',
+    author_email='mappa@googlegroups.com',
+    url='http://mappa.semagia.com/',
+    license='BSD',
+    packages=find_packages(),
+    namespace_packages=['mio'],
+    entry_points="""
+    [mio.reader]
+    jtm = mio.jtm
+    """,
+    platforms='any',
+    zip_safe=False,
+    include_package_data=True,
+    package_data={'': ['*.txt']},
+    install_requires=['tm>=0.1.7'],
+    keywords=['Topic Maps', 'Semantic Web', 'JTM', 'JSON Topic Maps'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        ]
+)
--- mio.jtm/tests/test_jtm.py
+++ mio.jtm/tests/test_jtm.py
@@ -12,8 +12,7 @@
 :organization: Semagia - http://www.semagia.com/
 :license:      BSD license
 """
-from mappaext.cxtm.cxtm_test import create_invalid_cxtm_cases, \
-    create_valid_cxtm_cases
+from mappaext.cxtm.cxtm_test import create_invalid_cxtm_cases, create_valid_cxtm_cases
 from mio.jtm import create_deserializer
 
 
