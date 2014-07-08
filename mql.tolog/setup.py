b4b90d19f1c4078402196b3a5ec0f3e394882265
Lars <Lars@localhost>
2014-07-08 23:05:54 +0200

    PEP8

--- mql.tolog/setup.py
+++ mql.tolog/setup.py
@@ -30,33 +30,33 @@ class sdist(_sdist):
 
 
 setup(
-      name = 'mql.tolog',
-      version = '0.1.0',
-      description = 'Topic Maps Query Language - tolog',
-      long_description = '\n\n'.join([open('README.txt').read(), open('CHANGES.txt').read()]),
-      author = 'Lars Heuer',
-      author_email = 'mappa@googlegroups.com',
-      url = 'http://mappa.semagia.com/',
-      license = 'BSD',
-      packages = find_packages(),
-      namespace_packages = ['mql'],
-      entry_points = """
-      """,
-      platforms = 'any',
-      zip_safe = False,
-      include_package_data = True,
-      package_data = {'': ['*.txt', '*.xsl']},
-      cmdclass={'sdist': sdist},
-      install_requires=['lxml>=2.3.1', 'tm>=0.1.7', 'mio.ctm>=0.1.3'],
-      keywords = ['Topic Maps', 'Semantic Web', 'tolog'],
-      classifiers = [
-                    'Intended Audience :: Developers',
-                    'Intended Audience :: Information Technology',
-                    'Topic :: Software Development',
-                    'Topic :: Software Development :: Libraries',
-                    'Topic :: Software Development :: Libraries :: Python Modules',
-                    'License :: OSI Approved :: BSD License',
-                    'Operating System :: OS Independent',
-                    'Programming Language :: Python',
-                    ]
-      )
+    name='mql.tolog',
+    version='0.1.0',
+    description='Topic Maps Query Language - tolog',
+    long_description='\n\n'.join([open('README.txt').read(), open('CHANGES.txt').read()]),
+    author='Lars Heuer',
+    author_email='mappa@googlegroups.com',
+    url='http://mappa.semagia.com/',
+    license='BSD',
+    packages=find_packages(),
+    namespace_packages=['mql'],
+    entry_points="""
+    """,
+    platforms='any',
+    zip_safe=False,
+    include_package_data=True,
+    package_data={'': ['*.txt', '*.xsl']},
+    cmdclass={'sdist': sdist},
+    install_requires=['lxml>=2.3.1', 'tm>=0.1.7', 'mio.ctm>=0.1.3'],
+    keywords=['Topic Maps', 'Semantic Web', 'tolog'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        ]
+)
