36e0802480bb438c787f12a4053f5d3532d1eea7
Lars Heuer <Lars Heuer@localhost>
2014-07-10 00:39:16 +0200

    Added test case, use nose tests

--- mio.ctm/tests/test_utils.py
+++ mio.ctm/tests/test_utils.py
@@ -12,112 +12,113 @@ Tests against mio.ctm.utils
 :organization: Semagia - http://www.semagia.com/
 :license:      BSD license
 """
-from nose.tools import eq_
+from nose.tools import eq_, ok_
 from mio.ctm.utils import *
 
 
 def test_is_native_datatype():
-    eq_(False, is_native_datatype(XSD.string))
-    eq_(True, is_native_datatype(XSD.integer))
-    eq_(True, is_native_datatype(u'http://www.w3.org/2001/XMLSchema#dateTime'))
-    eq_(True, is_native_datatype(u'http://psi.topicmaps.org/iso13250/ctm-integer'))
+    ok_(not is_native_datatype(XSD.string))
+    ok_(is_native_datatype(XSD.integer))
+    ok_(is_native_datatype(u'http://www.w3.org/2001/XMLSchema#dateTime'))
+    ok_(is_native_datatype(u'http://psi.topicmaps.org/iso13250/ctm-integer'))
 
 
 def test_keyword_valid():
-    eq_(True, is_keyword(u'isa'))
-    eq_(True, is_keyword(u'ako'))
-    eq_(True, is_keyword(u'def'))
-    eq_(True, is_keyword(u'end'))
+    ok_(is_keyword(u'isa'))
+    ok_(is_keyword(u'ako'))
+    ok_(is_keyword(u'def'))
+    ok_(is_keyword(u'end'))
 
 
 def test_keyword_invalid():
-    eq_(False, is_keyword(u'isa '))
-    eq_(False, is_keyword(u'ISA'))
+    ok_(not is_keyword(u'isa '))
+    ok_(not is_keyword(u'ISA'))
 
 
 def test_iri_valid():
     data = (u'http://www.semagia.com/',)
     for iri in data:
-        eq_(True, is_valid_iri(iri))
+        ok_(is_valid_iri(iri))
 
 
 def test_iri_invalid():
     data = (u'<http://www.semagia.com/>', u'http://{www.semagia.com/}',
             u'http:// www.semagia.com/',)
     for iri in data:
-        eq_(False, is_valid_iri(iri))
+        ok_(not is_valid_iri(iri))
 
 
 def test_valid_id_start():
     data = (u'_', u'A', u'a', u'ü', u'ä')
     for c in data:
-        eq_(True, is_valid_id_start(c))
+        ok_(is_valid_id_start(c))
 
 
 def test_invalid_id_start():
     data = (u'0', u'-', u'.')
     for c in data:
-        eq_(False, is_valid_id_start(c))
+        ok_(not is_valid_id_start(c))
 
 
 def test_valid_localid_start():
     data = (u'0', u'ü',)
     for c in data:
-        eq_(True, is_valid_localid_start(c))
+        ok_(is_valid_localid_start(c))
 
 
 def test_invalid_localid_start():
     data = (u'.', u'-')
     for c in data:
-        eq_(False, is_valid_localid_start(c))
+        ok_(not is_valid_localid_start(c))
 
 
 def test_valid_id_part():
     data = (u'-', u'.', u'0', u'a', u'ä', u'ö', u'ü', u'_')
     for c in data:
-        eq_(True, is_valid_id_part(c))
+        ok_(is_valid_id_part(c))
 
 
 def test_invalid_id_part():
     data = (u' ',)
     for c in data:
-        eq_(False, is_valid_id_part(c))
+        ok_(not is_valid_id_part(c))
 
 
 def test_valid_id():
-    data = (u'ident', u'_ident', u'ident.ifier', u'a1976-09-19', u'isa', u'öüä')
+    data = (u'ident', u'_ident', u'ident.ifier', u'a1976-09-19', u'isa',
+            u'öüä', u'MAIN-タイトル読み')
     for c in data:
-        eq_(True, is_valid_id(c))
+        ok_(is_valid_id(c))
 
 
 def test_invalid_id():
     data = (u'ident.', u'-ident', u'2ident.ifier', u'.isa')
     for c in data:
-        eq_(False, is_valid_id(c))
+        ok_(not is_valid_id(c))
 
 
 def test_valid_local_part():
     data = (u'1976-09-19', u'1semagia')
     for c in data:
-        eq_(True, is_valid_local_part(c))
+        ok_(is_valid_local_part(c))
 
 
 def test_invalid_local_part():
     data = (u'1976-09-19.', u'-semagia', u'.semagia', u'.1semagia')
     for c in data:
-        eq_(False, is_valid_local_part(c))
+        ok_(not is_valid_local_part(c))
 
 
 def test_valid_iri_part():
     data = (u'a', u')')
     for c in data:
-        eq_(True, is_valid_iri_part(c))
+        ok_(is_valid_iri_part(c))
 
 
 def test_invalid_iri_part():
     data = (u' ', u'"')
     for c in data:
-        eq_(False, is_valid_iri_part(c))
+        ok_(not is_valid_iri_part(c))
 
 
 def test_find_variables():
