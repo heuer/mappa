77556983c6a2486578adeae65d010d90af0ff34c
Lars <Lars@localhost>
2014-07-25 16:34:54 +0200

    Added __init__.py for tables.py, which was added by mistake

---
+++ mappa.store.ontopia/mappaext/store/ontopia/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2007 - 2014 -- Lars Heuer - Semagia <http://www.semagia.com/>.
+# All rights reserved.
+#
+# BSD license.
+#
+"""\
+
+
+:author:       Lars Heuer (heuer[at]semagia.com)
+:organization: Semagia - http://www.semagia.com/
+:license:      BSD license
+"""
