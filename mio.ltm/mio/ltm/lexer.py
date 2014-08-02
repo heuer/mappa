f53bbf134c362eab2c2bbe8e404594ff0d150eb6
Lars <Lars@localhost>
2014-08-02 10:55:34 +0200

    Moved utility function to lexer

--- mio.ltm/mio/ltm/lexer.py
+++ mio.ltm/mio/ltm/lexer.py
@@ -12,8 +12,8 @@ Linear Topic Maps Notation (LTM) 1.3 lexer.
 :organization: Semagia - http://www.semagia.com/
 :license:      BSD license
 """
+import re
 from tm.mio import MIOException
-from mio.ltm.utils import unescape_unicode
 
 directives = {
     'PREFIX': 'DIR_PREFIX',
@@ -97,3 +97,59 @@ def t_directive(t):
     if not t.type:
         raise MIOException('Unknown directive "%s"' % t.value)
     return t
+
+
+_ESC_UNICODE = re.compile(r'\\u([0-9A-Fa-f]{4,6})')
+
+def unescape_unicode(s):
+    """\
+    Unescapes a LTM string.
+
+    `s`
+        The string to unescape.
+    """
+    match = _ESC_UNICODE.search(s)
+    if not match:
+        return s
+    buff = []
+    pos = 0
+    while match:
+        buff.append(s[pos:match.start()])
+        start = match.start(1)
+        end = match.end()
+        pos = end
+        unicode_value = None
+        to_add = ''
+        try:
+            xx = match.group(1)
+            unicode_value = int(xx, 16)
+        except ValueError:
+            try:
+                xx = xx[:-1]
+                if len(xx) < 4:
+                    raise ValueError()
+                unicode_value = int(xx, 16)
+                to_add = s[end-1]
+            except ValueError:
+                try:
+                    xx = xx[:-1]
+                    if len(xx) < 4:
+                        raise ValueError()
+                    unicode_value = int(xx, 16)
+                    to_add = s[end-2:end]
+                except ValueError: # Invalid Unicode escape sequence
+                    buff.append(u'\\u')
+                    buff.append(s[start:end])
+                    pos = match.end()-3
+        if unicode_value is not None:
+            if unicode_value < 65536:
+                buff.append(unichr(unicode_value))
+            else:
+                high_surrogate = (unicode_value + 56557568) / 1024
+                low_surrogate = unicode_value + 56613888 - 1024 * high_surrogate
+                buff.append(unichr(high_surrogate))
+                buff.append(unichr(low_surrogate))
+            buff.append(to_add)
+        match = _ESC_UNICODE.search(s, pos)
+    buff.append(s[pos:])
+    return unicode(''.join(buff))
