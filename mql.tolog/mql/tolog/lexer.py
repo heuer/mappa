2b3a11f122ca8a761dde26fb1973fbda34c76f72
Lars <Lars@localhost>
2014-08-10 01:20:39 +0200

    PEP8

--- mql.tolog/mql/tolog/lexer.py
+++ mql.tolog/mql/tolog/lexer.py
@@ -37,11 +37,11 @@ _IDENT_PART = ur'%s|[\-0-9]|[\u00B7]|[\u0300-\u036F]|[\u203F-\u2040]' % _IDENT_S
 # Identifier
 _IDENT = ur'(%s)+(\.*(%s))*' % (_IDENT_START, _IDENT_PART)
 
-_DATE = r'\-?(000[1-9]|00[1-9][0-9]|0[1-9][0-9][0-9]|[1-9][0-9][0-9][0-9]+)\-(0[1-9]|1[0-2])\-(0[1-9]|1[0-9]|2[0-9]|3[0-1])'
+_DATE = ur'\-?(000[1-9]|00[1-9][0-9]|0[1-9][0-9][0-9]|[1-9][0-9][0-9][0-9]+)\-(0[1-9]|1[0-2])\-(0[1-9]|1[0-9]|2[0-9]|3[0-1])'
 # Timezone
-_TZ = r'Z|((\+|\-)[0-9]{2}:[0-9]{2})'
+_TZ = ur'Z|((\+|\-)[0-9]{2}:[0-9]{2})'
 # Time
-_TIME = r'[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)?(%s)?' % _TZ
+_TIME = ur'[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)?(%s)?' % _TZ
 
 _DIRECTIVES = {
     # tolog extensions
@@ -117,26 +117,26 @@ tokens = tuple(set(_RESERVED.values())) + tuple(_DIRECTIVES.values()) + (
 
 t_ignore = ' \t'
 
-t_EQ        = r'='
-t_NE        = r'/='
-t_LE        = r'<='
-t_LT        = r'<'
-t_GE        = r'>='
-t_GT        = r'>'
+t_EQ = ur'='
+t_NE = ur'/='
+t_LE = ur'<='
+t_LT = ur'<'
+t_GE = ur'>='
+t_GT = ur'>'
 
 
-t_LPAREN    = r'\('
-t_RPAREN    = r'\)'
-t_LCURLY    = r'\{'
-t_RCURLY    = r'\}'
-t_COMMA     = r','
-t_DOT       = r'\.'
-t_CIRCUMFLEX = r'\^'
-t_DOUBLE_CIRCUMFLEX = r'\^\^'
-t_IMPLIES   = r':-'
-t_COLON     = r':'
-t_PIPE_PIPE = r'\|{2}'
-t_PIPE      = r'\|'
+t_LPAREN = ur'\('
+t_RPAREN = ur'\)'
+t_LCURLY = ur'\{'
+t_RCURLY = ur'\}'
+t_COMMA = ur','
+t_DOT = ur'\.'
+t_CIRCUMFLEX = ur'\^'
+t_DOUBLE_CIRCUMFLEX = ur'\^\^'
+t_IMPLIES = ur':-'
+t_COLON = ur':'
+t_PIPE_PIPE = ur'\|{2}'
+t_PIPE = ur'\|'
 
 states = (
    ('tm', 'exclusive'),
@@ -151,29 +151,34 @@ def t_error(t):
 t_tm_error = t_error # Same error handling within state 'tm'
 
 def t_mlcomment(t):
-    r'/\*[^\*/]*\*/'
+    ur'/\*[^\*/]*\*/'
     t.lexer.lineno += t.value.count('\n')
 
+
 def t_comment(t):
-    r'\#[^\r\n]*'
+    ur'\#[^\r\n]*'
+
 
 def t_newline(t):
-    r'[\r\n]'
+    ur'[\r\n]'
     t.lexer.lineno += 1
 
+
 def t_QM(t):
-    r'\?'
+    ur'\?'
     # Everything after the ? is ignored
     t.lexer.lexpos = t.lexer.lexlen  # Lexer assumes that it has reached the end
     return t
 
+
 def t_STRING(t):
-    r'"([^"]|"{2})*"'
+    ur'"([^"]|"{2})*"'
     t.value = t.value[1:-1].replace('""', '"')
     return t
 
+
 def t_IRI(t):
-    r'<[^<>\"\{\}\`\\ ]+>'
+    ur'<[^<>\"\{\}\`\\ ]+>'
     t.value = t.value[1:-1]
     return t
 
@@ -192,15 +197,18 @@ def t_OID(t):
     t.value = t.value[1:]
     return t
 
+
 def t_SID(t):
     r'i"[^"]+"'
     t.value = t.value[2:-1]
     return t
 
+
 def t_SLO(t):
     r'a"[^"]+"'
     t.value = t.value[2:-1]
     return t
+
 
 def t_IID(t):
     r's"[^"]+"'
@@ -218,15 +226,16 @@ def t_QNAME(t):
 
 @TOKEN(_IDENT)
 def t_IDENT(t):
-    t.type = _RESERVED.get(t.value.lower(), 'IDENT')
-    if t.type == 'KW_INSERT':
+    t.type = _RESERVED.get(t.value.lower(), u'IDENT')
+    if t.type == u'KW_INSERT':
         t.lexer.begin('tm') # Switch to TM mode
-    elif t.type == 'KW_FROM' and not t.lexer.tolog_plus:
-        t.type = 'KW_WHERE'
+    elif t.type == u'KW_FROM' and not t.lexer.tolog_plus:
+        t.type = u'KW_WHERE'
     return t
 
+
 def t_directive(t):
-    r'%[A-Z\-a-z]+'
+    ur'%[A-Z\-a-z]+'
     t.type = _DIRECTIVES.get(t.value.lower())
     if not t.type:
         if t.value[:3] == u'%x-':
@@ -236,7 +245,7 @@ def t_directive(t):
             raise SyntaxQueryError('Unknown directive %s' % t.value)
     return t
 
-@TOKEN(r'%sT%s' % (_DATE, _TIME))
+@TOKEN(ur'%sT%s' % (_DATE, _TIME))
 def t_DATE_TIME(t):
     return t
 
@@ -244,12 +253,14 @@ def t_DATE_TIME(t):
 def t_DATE(t):
     return t
 
+
 def t_DECIMAL(t):
-    r'(\-|\+)?([0-9]+\.[0-9]+|\.([0-9])+)'
+    ur'(\-|\+)?([0-9]+\.[0-9]+|\.([0-9])+)'
     return t
 
+
 def t_INTEGER(t):
-    r'(\-|\+)?[0-9]+'
+    ur'(\-|\+)?[0-9]+'
     return t
 
 # Satisfy PLY and ignore nothing
@@ -259,15 +270,15 @@ t_tm_ignore = ''
 # for the time being although the tolog spec. says that it is an error since
 # 'from' is a tolog keyword.
 #TODO: Needs more work/tests
-_INTO_KEYWORD = re.compile(r'\s+(?=into\s+)', re.IGNORECASE).finditer
-_WHERE_KEYWORD = re.compile(r'\s+(?=(from|where)\s+(?!(.*?("(.*?(\.|;))|\.|(?:#\))))))', re.IGNORECASE).finditer
+_INTO_KEYWORD = re.compile(ur'\s+(?=into\s+)', re.IGNORECASE).finditer
+_WHERE_KEYWORD = re.compile(ur'\s+(?=(from|where)\s+(?!(.*?("(.*?(\.|;))|\.|(?:#\))))))', re.IGNORECASE).finditer
 
 # Matches first whitespace characters after INSERT
 # Then search for the tolog keyword 'into' or 'from'/'where' or to the
 # end of the string. The content between INSERT and from (exclusive) / end of 
 # string is returned as TM_FRAGMENT token
 def t_tm_content(t):
-    r'\s+'
+    ur'\s+'
     def last_match(match_iterator):
         m = None
         for m in match_iterator(t.lexer.lexdata, t.lexer.lexpos):
